3d05880f70735bf0400df83498466a03f1453add
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2010-11-30 14:18:11 +0100

    Corrected and improved timestep controls.

--- dune/porsol/blackoil/ComponentTransport.hpp
+++ dune/porsol/blackoil/ComponentTransport.hpp
@@ -43,6 +43,7 @@ public:
     void compute(const PhaseVec& phase_pressure, const CompVec& z)
     {
         BlackoilFluid::FluidState state = fluid_.computeState(phase_pressure, z);
+        saturation_ = state.saturation_;
         double total_mobility = 0.0;
         for (int phase = 0; phase < numPhases; ++phase) {
             total_mobility += state.mobility_[phase];
@@ -52,6 +53,11 @@ public:
         Dune::SharedFortranMatrix A(numComponents, numPhases, state.phase_to_comp_);
         Dune::SharedCMatrix cbp(numComponents, numPhases, &comp_by_phase_[0][0]);
         cbp = A; // Converting between orderings.
+    }
+
+    const PhaseVec& saturation() const
+    {
+        return saturation_;
     }
 
     const PhaseVec& fractionalFlow() const
@@ -65,6 +71,7 @@ public:
     }
 private:
     const BlackoilFluid& fluid_;
+    PhaseVec saturation_;
     PhaseVec fractional_flow_;
     ComponentInPhaseMatrix comp_by_phase_;
 };
@@ -91,13 +98,14 @@ public:
         int num_cells = grid.numCells();
         std::vector<CompVec> comp_change;
         std::vector<double> cell_outflux;
+        std::vector<double> cell_max_ff_deriv;
         double cur_time = 0.0;
         while (cur_time < dt) {
             updateFluidProperties(phase_pressure, comp_amount, external_pressure, external_composition, eos);
-            computeChange(grid, face_flux, comp_change, cell_outflux);
+            computeChange(grid, face_flux, comp_change, cell_outflux, cell_max_ff_deriv);
             double min_time = 1e100;
             for (int cell = 0; cell < num_cells; ++cell) {
-                double time = (rock.porosity(cell)*grid.cellVolume(cell))/cell_outflux[cell];
+                double time = (rock.porosity(cell)*grid.cellVolume(cell))/(cell_outflux[cell]*cell_max_ff_deriv[cell]);
                 min_time = std::min(time, min_time);
             }
             double step_time = dt - cur_time;
@@ -117,9 +125,10 @@ public:
 
 
 private: // Data
-
+    PhaseVec bdy_saturation_;
     PhaseVec bdy_fractional_flow_;
     typename EquationOfState::ComponentInPhaseMatrix bdy_comp_in_phase_;
+    std::vector<PhaseVec> saturation_;
     std::vector<PhaseVec> fractional_flow_;
     std::vector<typename EquationOfState::ComponentInPhaseMatrix> comp_in_phase_;
 
@@ -133,14 +142,17 @@ private: // Methods
                                EquationOfState& eos)
     {
         int num_cells = phase_pressure.size();
+        saturation_.resize(num_cells);
         fractional_flow_.resize(num_cells);
         comp_in_phase_.resize(num_cells);
         for (int cell = 0; cell < num_cells; ++cell) {
             eos.compute(phase_pressure[cell], comp_amount[cell]);
+            saturation_[cell] = eos.saturation();
             fractional_flow_[cell] = eos.fractionalFlow();
             comp_in_phase_[cell] = eos.compositionByPhase();
         }
         eos.compute(external_pressure, external_composition);
+        bdy_saturation_ = eos.saturation();
         bdy_fractional_flow_ = eos.fractionalFlow();
         bdy_comp_in_phase_ = eos.compositionByPhase();
     }
@@ -150,33 +162,55 @@ private: // Methods
     void computeChange(const Grid& grid,
                        const std::vector<double>& face_flux,
                        std::vector<CompVec>& comp_change,
-                       std::vector<double>& cell_outflux)
+                       std::vector<double>& cell_outflux,
+                       std::vector<double>& cell_max_ff_deriv)
     {
         comp_change.clear();
         CompVec zero(0.0);
         comp_change.resize(grid.numCells(), zero);
         cell_outflux.clear();
         cell_outflux.resize(grid.numCells(), 0.0);
+        cell_max_ff_deriv.clear();
+        cell_max_ff_deriv.resize(grid.numCells(), 0.0);
         for (int face = 0; face < grid.numFaces(); ++face) {
             int c0 = grid.faceCell(face, 0);
             int c1 = grid.faceCell(face, 1);
+            int upwind_cell = (face_flux[face] > 0.0) ? c0 : c1;
+            int downwind_cell = (face_flux[face] > 0.0) ? c1 : c0;
+            PhaseVec upwind_sat = upwind_cell < 0 ? bdy_saturation_ : saturation_[upwind_cell];
+            PhaseVec upwind_ff = upwind_cell < 0 ? bdy_fractional_flow_ : fractional_flow_[upwind_cell];
+            PhaseVec phase_flux(upwind_ff);
+            phase_flux *= face_flux[face];
             CompVec change(0.0);
+            double face_max_ff_deriv = 0.0;
             for (int phase = 0; phase < numPhases; ++phase) {
-                int upwind_cell = (face_flux[face] > 0.0) ? c0 : c1;
-                double ff = upwind_cell < 0 ? bdy_fractional_flow_[phase] : fractional_flow_[upwind_cell][phase];
-                double phase_flux = face_flux[face]*ff;
+                // Compute z change.
                 CompVec z_in_phase = upwind_cell < 0 ? bdy_comp_in_phase_[phase] : comp_in_phase_[upwind_cell][phase];
-                z_in_phase *= phase_flux;
+                z_in_phase *= phase_flux[phase];
                 change += z_in_phase;
-                if (upwind_cell >= 0) {
-                    cell_outflux[upwind_cell] += std::fabs(phase_flux);
+                // Estimate derivative of fractional flow.
+                double ff_deriv = 0.0;
+                if (downwind_cell >= 0) { // Only contribution on inflow and internal faces.
+                    double ff_diff = upwind_ff[phase] - fractional_flow_[downwind_cell][phase];
+                    if (std::fabs(ff_diff) < 1e-14) {
+                        ff_deriv = 0.0; // No contribution if identical saturations.
+                    } else {
+                        ff_deriv = ff_diff/(upwind_sat[phase] - saturation_[downwind_cell][phase]);
+                    }
                 }
+                ASSERT(ff_deriv >= 0.0);
+                face_max_ff_deriv = std::max(face_max_ff_deriv, ff_deriv);
+            }
+            if (upwind_cell >= 0) {
+                cell_outflux[upwind_cell] += std::fabs(face_flux[face]);
             }
             if (c0 >= 0) {
                 comp_change[c0] -= change;
+                cell_max_ff_deriv[c0] = std::max(cell_max_ff_deriv[c0], face_max_ff_deriv);
             }
             if (c1 >= 0) {
                 comp_change[c1] += change;
+                cell_max_ff_deriv[c1] = std::max(cell_max_ff_deriv[c1], face_max_ff_deriv);
             }
         }
     }
