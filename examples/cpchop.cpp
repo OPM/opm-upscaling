07779105ad923a93deab5f5fa5db5d0d14a82451
Kari B. Skjerve <karbor@statoil.com>
2011-08-15 13:26:46 +0200

    Added capillary pressure upscaling option

--- examples/cpchop.cpp
+++ examples/cpchop.cpp
@@ -20,6 +20,7 @@
 
 #include <dune/common/CornerpointChopper.hpp>
 #include <dune/upscaling/SinglePhaseUpscaler.hpp>
+#include <dune/common/MonotCubicInterpolator.hpp>
 #include <dune/porsol/common/setupBoundaryConditions.hpp>
 #include <dune/common/Units.hpp>
 
@@ -35,6 +36,7 @@
 #include <sstream>
 #include <fstream>
 #include <iostream>
+#include <cfloat>
 
 
 int main(int argc, char** argv)
@@ -44,7 +46,8 @@ int main(int argc, char** argv)
         std::cout << "       [zlen=5] [imin=] [imax=] [jmin=] [jmax=] [upscale=true] [bc=fixed]" << std::endl;
         std::cout << "       [resettoorigin=true] [seed=111] [z_tolerance=0.0] [minperm=1e-9] " << std::endl;
         std::cout << "       [dips=false] [satnumvolumes=false] [mincellvolume=1e-9]" << std::endl;
-        std::cout << "       [filebase=] [resultfile=] [endpoints=false] [rock_list=]" << std::endl;
+        std::cout << "       [filebase=] [resultfile=] [endpoints=false] [cappres=false]" << std::endl;
+        std::cout << "       [rock_list=] [anisotropicrocks=false]" << std::endl;
         exit(1);
     }
     Dune::parameter::ParameterGroup param(argc, argv);
@@ -82,10 +85,28 @@ int main(int argc, char** argv)
 
     bool satnumvolumes = param.getDefault("satnumvolumes", false); // whether to count volumes pr. satnum
 
+    // upscaling of endpoints and capillary pressure
+    // Conversion factor, multiply mD numbers with this to get m² numbers
+    const double milliDarcyToSqMetre = 9.869233e-16;
+    // Input for surfaceTension is dynes/cm, SI units are Joules/square metre
+    const double surfaceTension = param.getDefault("surfaceTension", 11.0) * 1e-3; // multiply with 10^-3 to obtain SI units 
+
     bool endpoints = param.getDefault("endpoints", false); // whether to upscale saturation endpoints
-    std::string rock_list =  param.getDefault<std::string>("rock_list", "no_list");
-    //std::vector<Dune::RockAnisotropicRelperm> rock_;
+    bool cappres = param.getDefault("cappres", false); // whether to upscale capillary pressure
+    if (cappres) { endpoints = true; }
+    std::string rock_list = param.getDefault<std::string>("rock_list", "no_list");
+    bool anisorocks = param.getDefault("anisotropicrocks", false);
     std::vector<std::vector<double> > rocksatendpoints_;
+    std::vector<std::vector<double> > jfuncendpoints_; // Used if isotropic rock input
+    int nsatpoints = 5; // nuber of saturation points in upscaled capillary pressure function per subsample
+    double saturationThreshold = 0.00001;
+
+    // For isotropic input rocks:
+    std::vector<MonotCubicInterpolator> InvJfunctions; // Holds the inverse of the loaded J-functions.
+    // For anisotropic input rocks:
+    std::vector<MonotCubicInterpolator> SwPcfunctions; // Holds Sw(Pc) for each rocktype.
+
+    // Read rock data from files specifyed in rock_list
     if (endpoints) {
         if (!rock_list.compare("no_list")) {
             std::cout << "Can't do endponts without rock list (" << rock_list << ")" << std::endl;
@@ -100,7 +121,8 @@ int main(int argc, char** argv)
         rl >> num_rocks;
         ASSERT(num_rocks >= 1);
         rocksatendpoints_.resize(num_rocks);
-        
+        jfuncendpoints_.resize(num_rocks);
+        // Loop through rock files defined in rock_list and store the data we need
         for (int i = 0; i < num_rocks; ++i) {
             std::string spec;
             while (spec.empty()) {
@@ -111,60 +133,66 @@ int main(int argc, char** argv)
 	    std::string rockname;
 	    specstream >> rockname;
             std::string rockfilename = rockname;
+
             std::ifstream rock_stream(rockfilename.c_str());
             if (!rock_stream) {
                 THROW("Could not open file " << rockfilename);
             }
-                        char c = rock_stream.peek();
-            while ((c == '-' || c == '#') && rock_stream) {
-                if (c == '#') {
-                    std::string commentline;
-                    std::getline(rock_stream, commentline);
-                    c = rock_stream.peek();
+            
+            if (! anisorocks) { //Isotropic input rocks (Sw Krw Kro J)
+                MonotCubicInterpolator Jtmp;
+                try {
+                    Jtmp = MonotCubicInterpolator(rockname, 1, 4); 
                 }
-                else { /* Check if the initial '-' was first byte of the sequence '--' */
-                    rock_stream.get(c);
-                    if (c == '-') {
-                        std::string commentline;
-                        std::getline(rock_stream, commentline);
-                        c = rock_stream.peek();
-                    } else {
-                        rock_stream.putback(c);
+                catch (const char * errormessage) {
+                    std::cerr << "Error: " << errormessage << std::endl;
+                    std::cerr << "Check filename" << std::endl;
+                    exit(1);
+                }
+                
+                // Invert J-function, now we get saturation as a function of pressure:
+                if (Jtmp.isStrictlyMonotone()) {
+                    InvJfunctions.push_back(MonotCubicInterpolator(Jtmp.get_fVector(), Jtmp.get_xVector()));
+                }
+                else {
+                    std::cerr << "Error: Jfunction " << i+1 << " in rock file " << rockname << " was not invertible." << std::endl;
+                    exit(1);
+                }
+                
+                jfuncendpoints_[i][0] = Jtmp.getMinimumX().second;
+                jfuncendpoints_[i][1] = Jtmp.getMaximumX().second;
+                rocksatendpoints_[i][0] = Jtmp.getMinimumX().first;
+                rocksatendpoints_[i][1] = Jtmp.getMaximumX().first;
+                if (rocksatendpoints_[i][0] < 0 || rocksatendpoints_[i][0] > 1) {
+                    THROW("Minimum rock saturation (" << rocksatendpoints_[i][0] << ") not sane for rock " 
+                          << rockfilename << "." << std::endl << "Did you forget to specify anisotropicrocks=true ?");  
+                }
+            }
+            else { //Anisotropic input rocks (Pc Sw Krxx Kryy Krzz)
+                MonotCubicInterpolator Pctmp;
+                try {
+                    Pctmp = MonotCubicInterpolator(rockname, 2, 1);
+                }
+                catch (const char * errormessage) {
+                    std::cerr << "Error: " << errormessage << std::endl;
+                    std::cerr << "Check filename and columns 1 and 2 (Pc and Sw)" << std::endl;
+                    exit(1);
+                }
+                if (cappres) {
+                    // Invert Pc(Sw) curve into Sw(Pc):
+                    if (Pctmp.isStrictlyMonotone()) {
+                        SwPcfunctions.push_back(MonotCubicInterpolator(Pctmp.get_fVector(), Pctmp.get_xVector()));
+                    }
+                    else {
+                        std::cerr << "Error: Pc(Sw) curve " << i+1 << " in rock file " << rockname << " was not invertible." << std::endl;
+                        exit(1);
                     }
                 }
-            }
-            if (!rock_stream) {
-                THROW("Something went wrong while skipping optional comment header of rock file.");
-            }
-	    typedef Dune::FieldVector<double, 2> Data;
-	    std::istream_iterator<Data> start(rock_stream);
-	    std::istream_iterator<Data> end;
-	    std::vector<Data> data(start, end);
-	    if (!rock_stream.eof()) {
-		THROW("Reading stopped but we're not at eof: something went wrong reading data of rock file.");
-	    }
-            
-            double swmin = std::min(data[0][1],data[data.size()-1][1]);
-            double swmax = std::max(data[0][1],data[data.size()-1][1]);
-            rocksatendpoints_[i][0] = swmin;
-            rocksatendpoints_[i][1] = swmax;
-
-
-            // This part might be useful if one wants to implement relative permeability in cpchop
-// 	    std::vector<double> pc, sw;
-// 	    for (int i = 0; i < int(data.size()); ++i) {
-// 		sw.push_back(data[i][1]);
-// 		pc.push_back(data[i][0]);
-// 	    }
-
-//             typedef Dune::utils::NonuniformTableLinear<double> TabFunc;
-//             TabFunc pc_ = TabFunc(sw, pc);
-            
-            //rocksatendpoints_[i] = 
-            //rock_[i].read(dir, spec);
+                rocksatendpoints_[i][0] = Pctmp.getMinimumX().first;
+                rocksatendpoints_[i][1] = Pctmp.getMaximumX().first;
+            }          
         }
     }
-
 
     double z_tolerance = param.getDefault("z_tolerance", 0.0);
     double residual_tolerance = param.getDefault("residual_tolerance", 1e-8);
@@ -233,6 +261,7 @@ int main(int argc, char** argv)
     std::vector<double> permxzs;
     std::vector<double> permxys;
     std::vector<double> minsws, maxsws;
+    std::vector<std::vector<double> > pcvalues;
     std::vector<double> xdips, ydips;
 
     // Initialize a matrix for subsample satnum volumes. 
@@ -286,6 +315,7 @@ int main(int argc, char** argv)
                 // Calculate minimum and maximum water volume in each cell
                 // Create single-phase upscaling object to get poro and perm values from the grid
                 Dune::EclipseGridParser subparser = ch.subparser();
+                std::vector<double>  perms = subparser.getFloatingPointValue("PERMX");
                 subparser.convertToSI();
                 Dune::SinglePhaseUpscaler upscaler;                
                 upscaler.init(subparser, bctype, minpermSI, z_tolerance,
@@ -296,7 +326,10 @@ int main(int argc, char** argv)
                 cellVolumes.resize(satnums.size(), 0.0);
                 cellPoreVolumes.resize(satnums.size(), 0.0);
                 int tesselatedCells = 0;
-                double Swirvolume = 0, Sworvolume = 0;
+                double Pcmax = -DBL_MAX, Pcmin = DBL_MAX;
+                //double maxSinglePhasePerm = 0;
+                double Swirvolume = 0;
+                double Sworvolume = 0;
                 const std::vector<int>& ecl_idx = upscaler.grid().globalCell();
                 Dune::CpGrid::Codim<0>::LeafIterator c = upscaler.grid().leafbegin<0>();
                 for (; c != upscaler.grid().leafend<0>(); ++c) {
@@ -304,19 +337,116 @@ int main(int argc, char** argv)
                     if (satnums[cell_idx] > 0) { // Satnum zero is "no rock"
                         cellVolumes[cell_idx] = c->geometry().volume();
                         cellPoreVolumes[cell_idx] = cellVolumes[cell_idx] * poros[cell_idx];
-                        Swirvolume += rocksatendpoints_[satnums[cell_idx]-1][0] * cellPoreVolumes[cell_idx]; 
-                        Sworvolume += rocksatendpoints_[satnums[cell_idx]-1][1] * cellPoreVolumes[cell_idx];
-                        
+                        double Pcmincandidate, Pcmaxcandidate, minSw, maxSw;
+                        if (!anisorocks) {
+                            if (cappres) {
+                                Pcmincandidate = jfuncendpoints_[int(satnums[cell_idx])-1][1]
+                                    / sqrt(perms[cell_idx] * milliDarcyToSqMetre/poros[cell_idx]) * surfaceTension;
+                                Pcmaxcandidate = jfuncendpoints_[int(satnums[cell_idx])-1][0]
+                                    / sqrt(perms[cell_idx] * milliDarcyToSqMetre/poros[cell_idx]) * surfaceTension;
+                            }
+                            minSw = rocksatendpoints_[int(satnums[cell_idx])-1][0];
+                            maxSw = rocksatendpoints_[int(satnums[cell_idx])-1][1];
+                        }
+                        else { // anisotropic input, we do not to J-function scaling
+                            if (cappres) {
+                                Pcmincandidate = SwPcfunctions[int(satnums[cell_idx])-1].getMinimumX().first;
+                                Pcmaxcandidate = SwPcfunctions[int(satnums[cell_idx])-1].getMaximumX().first;
+                            }
+                            minSw = rocksatendpoints_[int(satnums[cell_idx])-1][0];
+                            maxSw = rocksatendpoints_[int(satnums[cell_idx])-1][1];
+                        }
+                        if (cappres) {
+                            Pcmin = std::min(Pcmincandidate, Pcmin);
+                            Pcmax = std::max(Pcmaxcandidate, Pcmax);
+                        }
+                        Swirvolume += minSw * cellPoreVolumes[cell_idx];
+                        Sworvolume += maxSw * cellPoreVolumes[cell_idx];
                     }
-                    ++ tesselatedCells;
+                    ++tesselatedCells; // keep count.
                 }
-                
+
+                // Total porevolume and total volume -> upscaled porosity:
                 double poreVolume = std::accumulate(cellPoreVolumes.begin(), 
                                                     cellPoreVolumes.end(),
                                                     0.0);
-                minsws.push_back(Swirvolume/poreVolume);
-                maxsws.push_back(Sworvolume/poreVolume);
+                double Swir = Swirvolume/poreVolume;
+                double Swor = Sworvolume/poreVolume;
+                minsws.push_back(Swir);
+                maxsws.push_back(Swor);
+                if (cappres) {
+                    // Upscale capillary pressure function
+                    MonotCubicInterpolator WaterSaturationVsCapPressure;
+                    double largestSaturationInterval = Swor-Swir;
+                    double Ptestvalue = Pcmax;
+                    while (largestSaturationInterval > (Swor-Swir)/double(nsatpoints)) {
+                        if (Pcmax == Pcmin) {
+                            // This is a dummy situation, we go through once and then 
+                            // we are finished (this will be triggered by zero permeability)
+                            Ptestvalue = Pcmin;
+                            largestSaturationInterval = 0;
+                        }
+                        else if (WaterSaturationVsCapPressure.getSize() == 0) {
+                            /* No data values previously computed */
+                            Ptestvalue = Pcmax;
+                        }
+                        else if (WaterSaturationVsCapPressure.getSize() == 1) {
+                            /* If only one point has been computed, it was for Pcmax. So now
+                               do Pcmin */
+                            Ptestvalue = Pcmin;
+                        }
+                        else {
+                            /* Search for largest saturation interval in which there are no
+                               computed saturation points (and estimate the capillary pressure
+                               that will fall in the center of this saturation interval)
+                            */
+                            std::pair<double,double> SatDiff = WaterSaturationVsCapPressure.getMissingX();
+                            Ptestvalue = SatDiff.first;
+                            largestSaturationInterval = SatDiff.second;
+                        }
+                        // Check for saneness of Ptestvalue:
+                        if (isnan(Ptestvalue) | isinf(Ptestvalue)) {
+                            std::cerr << "ERROR: Ptestvalue was inf or nan" << std::endl;
+                            break; // Jump out of while-loop, just print out the results
+                            // up to now and exit the program
+                        }
+
+                        double waterVolume = 0.0;
+                        for (unsigned int i = 0; i < ecl_idx.size(); ++i) {
+                            unsigned int cell_idx = ecl_idx[i];
+                            double waterSaturationCell = 0.0;
+                            if (satnums[cell_idx] > 0) { // handle "no rock" cells with satnum zero
+                                double PtestvalueCell;
+                                
+                                PtestvalueCell = Ptestvalue;
+                                
+                                if (!anisorocks) {   
+                                    double Jvalue = sqrt(perms[cell_idx] * milliDarcyToSqMetre /poros[cell_idx]) * PtestvalueCell / surfaceTension;
+                                    waterSaturationCell 
+                                        = InvJfunctions[int(satnums[cell_idx])-1].evaluate(Jvalue);
+                                }
+                                else { // anisotropic_input, then we do not do J-function-scaling
+                                    waterSaturationCell = SwPcfunctions[int(satnums[cell_idx])-1].evaluate(PtestvalueCell);
+                                }
+                            }
+                            waterVolume += waterSaturationCell  * cellPoreVolumes[cell_idx];
+                        }
+                        WaterSaturationVsCapPressure.addPair(Ptestvalue, waterVolume/poreVolume);
+                    }
+                    WaterSaturationVsCapPressure.chopFlatEndpoints(saturationThreshold);
+                    std::vector<double> wattest = WaterSaturationVsCapPressure.get_fVector();
+                    std::vector<double> cprtest = WaterSaturationVsCapPressure.get_xVector();
+                    MonotCubicInterpolator CapPressureVsWaterSaturation(WaterSaturationVsCapPressure.get_fVector(), 
+                                                                        WaterSaturationVsCapPressure.get_xVector());
+                    std::vector<double> pcs;
+                    for (uint satp=0; satp<nsatpoints; ++satp) {
+                        pcs.push_back(CapPressureVsWaterSaturation.evaluate(Swir+(Swor-Swir)/(nsatpoints-1)*satp));
+                    }
+                    pcvalues.push_back(pcs);
+                }
+                
             }
+
 
 	    if (dips) {
 		Dune::EclipseGridParser subparser = ch.subparser();
@@ -376,6 +506,7 @@ int main(int argc, char** argv)
         }
 
     }
+    
 
     // Make stream of output data, to be outputted to screen and optionally to file
     std::stringstream outputtmp;
@@ -413,6 +544,9 @@ int main(int argc, char** argv)
     }
     if (endpoints) {
         outputtmp << "                  Swir                    Swor";
+        if (cappres) {
+            outputtmp << "                  Pc(Swir)                Pc2                     Pc3                     Pc4                     Pc(Swor)";            
+        }
     }
     if (dips) {
 	outputtmp << "              xdip              ydip";
@@ -444,6 +578,14 @@ int main(int argc, char** argv)
 	    outputtmp <<
 		std::showpoint << std::setw(fieldwidth) << std::setprecision(outputprecision) << minsws[sample-1] << '\t' <<
 		std::showpoint << std::setw(fieldwidth) << std::setprecision(outputprecision) << maxsws[sample-1];
+            if (cappres) {
+                outputtmp <<
+                    std::showpoint << std::setw(fieldwidth) << std::setprecision(outputprecision) << pcvalues[sample-1][0] << '\t' <<
+                    std::showpoint << std::setw(fieldwidth) << std::setprecision(outputprecision) << pcvalues[sample-1][1] << '\t' <<
+                    std::showpoint << std::setw(fieldwidth) << std::setprecision(outputprecision) << pcvalues[sample-1][2] << '\t' <<
+                    std::showpoint << std::setw(fieldwidth) << std::setprecision(outputprecision) << pcvalues[sample-1][3] << '\t' <<
+                    std::showpoint << std::setw(fieldwidth) << std::setprecision(outputprecision) << pcvalues[sample-1][4];
+            }
 	}
 	if (dips) {
 	    outputtmp <<
