8ee3058af9bd8bf1e05e9cf5fc4d4688bcf65ff8
Roland Kaufmann <roland.kaufmann@uni.no>
2013-03-05 14:41:49 +0100

    Various improvements in the build process
    Allow uppercase alternatives, fix whitespace bug, allow empty pre-
    compiled header, find OpenMP

--- cmake/Modules/OpmPackage.cmake
+++ cmake/Modules/OpmPackage.cmake
@@ -26,6 +26,7 @@
 #   ${module}_LIBRARIES       Directory of shared object files
 #   ${module}_DEFINITIONS     Defines that must be set to compile
 #   ${module}_CONFIG_VARS     List of defines that should be in config.h
+#	${module}_QUIET           Verbosity of last find of this module
 #   HAVE_${MODULE}            Binary value to use in config.h
 #
 # Note: Arguments should be quoted, otherwise a list will spill into the
@@ -36,16 +37,7 @@
 
 # <http://www.vtk.org/Wiki/CMake:How_To_Find_Libraries>
 
-# libraries should always be trimmed from the beginning, so that also
-# missing functions in those later in the list will be resolved
-macro (remove_duplicate_libraries module)
-  if (DEFINED ${module}_LIBRARIES)
-	list (REVERSE ${module}_LIBRARIES)
-	list (REMOVE_DUPLICATES ${module}_LIBRARIES)
-	list (REVERSE ${module}_LIBRARIES)
-  endif (DEFINED ${module}_LIBRARIES)
-endmacro (remove_duplicate_libraries module)
-
+include (Duplicates)
 # append all items from src into dst; both must be *names* of lists
 macro (append_found src dst)
   foreach (_item IN LISTS ${src})
@@ -56,6 +48,13 @@ macro (append_found src dst)
 endmacro (append_found src dst)
 
 function (find_opm_package module deps header lib defs prog conf)
+  # variables to pass on to other packages
+  if (FIND_QUIETLY)
+	set (${module}_QUIET "QUIET")
+  else (FIND_QUIETLY)
+	set (${module}_QUIET "")
+  endif (FIND_QUIETLY)
+
   # if someone else has included this test, don't do it again
   if (${${module}_FOUND})
 	return ()
@@ -80,12 +79,17 @@ function (find_opm_package module deps header lib defs prog conf)
   set (${module}_DEFINITIONS ${PkgConf_${module}_CFLAGS_OTHER})
   set (${module}_LINKER_FLAG ${PkgConf_${module}_LDFLAGS_OTHER})
 
+  # in addition to accepting mod-ule_ROOT, we also accept the somewhat
+  # more idiomatic MOD_ULE_ROOT variant
+  string (TOUPPER ${module} MODULE_UPPER)
+  string (REPLACE "-" "_" MODULE ${MODULE_UPPER})
+
   # if the user hasn't specified any location, and it isn't found
   # in standard system locations either, then start to wander
   # about and look for it in proximity to ourself. Qt Creator likes
   # to put the build-directories as siblings to the source trees,
   # but with a -build suffix
-  if (NOT (${module}_DIR OR ${module}_ROOT))
+  if (NOT (${module}_DIR OR ${module}_ROOT OR ${MODULE}_ROOT))
 	string (TOLOWER "${module}" _module_lower)
 	set (_guess
 	  "../${module}"
@@ -97,7 +101,7 @@ function (find_opm_package module deps header lib defs prog conf)
 	foreach (_item IN ITEMS ${_guess})
 	  list (APPEND _guess_bin "${PROJECT_BINARY_DIR}/${_item}")
 	endforeach (_item)
-  endif (NOT (${module}_DIR OR ${module}_ROOT))
+  endif (NOT (${module}_DIR OR ${module}_ROOT OR ${MODULE}_ROOT))
 
   # search for this include and library file to get the installation
   # directory of the package; hints are searched before the system locations,
@@ -105,7 +109,7 @@ function (find_opm_package module deps header lib defs prog conf)
   find_path (${module}_INCLUDE_DIR
 	NAMES "${header}"
 	PATHS ${_guess}
-	HINTS ${${module}_DIR} ${${module}_ROOT} ${PkgConf_${module}_INCLUDE_DIRS}
+	HINTS ${${module}_DIR} ${${module}_ROOT} ${${MODULE}_ROOT} ${PkgConf_${module}_INCLUDE_DIRS}
 	PATH_SUFFIXES "include"
 	)
 
@@ -114,7 +118,7 @@ function (find_opm_package module deps header lib defs prog conf)
 	find_library (${module}_LIBRARY
 	  NAMES "${lib}"
 	  PATHS ${_guess_bin}
-	  HINTS ${${module}_DIR} ${${module}_ROOT} ${PkgConf_${module}_LIBRARY_DIRS}
+	  HINTS ${${module}_DIR} ${${module}_ROOT} ${${MODULE}_ROOT} ${PkgConf_${module}_LIBRARY_DIRS}
 	  PATH_SUFFIXES "lib" "lib/.libs" ".libs" "lib32" "lib64" "lib/${CMAKE_LIBRARY_ARCHITECTURE}"
 	  )
   else (NOT "${lib}" STREQUAL "")
@@ -175,13 +179,11 @@ function (find_opm_package module deps header lib defs prog conf)
   include (CheckCXXSourceCompiles)
   # only add these if they are actually found; otherwise it won't
   # compile and the variable won't be set
-  append_found (${module}_INCLUDE_DIR CMAKE_REQUIRED_INCLUDES)
+  append_found (${module}_INCLUDE_DIRS CMAKE_REQUIRED_INCLUDES)
   append_found (${module}_LIBRARIES CMAKE_REQUIRED_LIBRARIES)
   # since we don't have any config.h yet
   list (APPEND CMAKE_REQUIRED_DEFINITIONS ${${module}_DEFINITIONS})
   list (APPEND CMAKE_REQUIRED_DEFINITIONS ${${module}_CMD_CONFIG})
-  string (TOUPPER ${module} MODULE)
-  string (REPLACE "-" "_" MODULE ${MODULE})
   check_cxx_source_compiles ("${prog}" HAVE_${MODULE})
   cmake_pop_check_state ()
 
@@ -195,7 +197,7 @@ function (find_opm_package module deps header lib defs prog conf)
   find_package_handle_standard_args (
 	${module}
 	DEFAULT_MSG
-	${module}_INCLUDE_DIR ${_lib_var}
+	${module}_INCLUDE_DIR ${_lib_var} HAVE_${MODULE}
 	)
 
   # allow the user to override these from user interface
@@ -204,7 +206,6 @@ function (find_opm_package module deps header lib defs prog conf)
 
   # some genius that coded the FindPackageHandleStandardArgs figured out
   # that the module name should be in uppercase (?!)
-  string (TOUPPER ${module} MODULE_UPPER)
   set (${module}_FOUND "${${MODULE_UPPER}_FOUND}" PARENT_SCOPE)
 
   # return these variables to the caller
@@ -213,6 +214,7 @@ function (find_opm_package module deps header lib defs prog conf)
   set (${module}_DEFINITIONS "${${module}_DEFINITIONS}" PARENT_SCOPE)
   set (${module}_CONFIG_VARS "${${module}_CONFIG_VARS}" PARENT_SCOPE)
   set (${module}_LINKER_FLAGS "${${module}_LINKER_FLAGS}" PARENT_SCOPE)
+  set (${module}_QUIET "${${module}_QUIET}" PARENT_SCOPE)
   set (HAVE_${MODULE} "${HAVE_${MODULE}}" PARENT_SCOPE)
 endfunction (find_opm_package module deps header lib defs prog conf)
 
@@ -223,6 +225,8 @@ function (debug_find_vars module)
   message (STATUS "${module}_LIBRARIES    = ${${module}_LIBRARIES}")
   message (STATUS "${module}_DEFINITIONS  = ${${module}_DEFINITIONS}")
   message (STATUS "${module}_CONFIG_VARS  = ${${module}_CONFIG_VARS}")
+  message (STATUS "${module}_LINKER_FLAGS = ${${module}_LINKER_FLAGS}")
+  message (STATUS "${module}_QUIET        = ${${module}_QUIET}")
   string (TOUPPER ${module} MODULE)
   string (REPLACE "-" "_" MODULE ${MODULE})  
   message (STATUS "HAVE_${MODULE}         = ${HAVE_${MODULE}}")
