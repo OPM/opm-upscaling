31d81f8d3067c68f4ed6946dbd9ad8563405a301
Kari B. Skjerve <karbor@statoil.com>
2013-10-09 12:34:36 +0200

    untabified UpscalerBase.hpp and UpscalerBase_impl.hpp

--- opm/upscaling/UpscalerBase_impl.hpp
+++ opm/upscaling/UpscalerBase_impl.hpp
@@ -46,12 +46,12 @@ namespace Opm
 
     template <class Traits>
     inline UpscalerBase<Traits>::UpscalerBase()
-	: bctype_(Fixed),
-	  twodim_hack_(false),
-	  residual_tolerance_(1e-8),
-	  linsolver_maxit_(0),
-	  linsolver_prolongate_factor_(1.0),
-	  linsolver_verbosity_(0),
+        : bctype_(Fixed),
+          twodim_hack_(false),
+          residual_tolerance_(1e-8),
+          linsolver_maxit_(0),
+          linsolver_prolongate_factor_(1.0),
+          linsolver_verbosity_(0),
           linsolver_type_(3),
           linsolver_smooth_steps_(1)
     {
@@ -63,8 +63,8 @@ namespace Opm
     template <class Traits>
     inline void UpscalerBase<Traits>::init(const Opm::parameter::ParameterGroup& param)
     {
-	initImpl(param);
-	initFinal(param);
+        initImpl(param);
+        initFinal(param);
     }
 
 
@@ -79,15 +79,15 @@ namespace Opm
         // to constructing the grid and associated properties.
         //
         int bct = param.get<int>("boundary_condition_type");
-	bctype_ = static_cast<BoundaryConditionType>(bct);
+        bctype_ = static_cast<BoundaryConditionType>(bct);
 
         // Import control parameters pertaining to reduced physical
         // dimensionality ("2d_hack = true" precludes periodic
         // boundary conditions in the Z direction), and linear solves.
         //
         twodim_hack_ = param.getDefault("2d_hack", twodim_hack_);
-	residual_tolerance_ = param.getDefault("residual_tolerance", residual_tolerance_);
-	linsolver_verbosity_ = param.getDefault("linsolver_verbosity", linsolver_verbosity_);
+        residual_tolerance_ = param.getDefault("residual_tolerance", residual_tolerance_);
+        linsolver_verbosity_ = param.getDefault("linsolver_verbosity", linsolver_verbosity_);
         linsolver_type_ = param.getDefault("linsolver_type", linsolver_type_);
         linsolver_maxit_ = param.getDefault("linsolver_max_iterations", linsolver_maxit_);
         linsolver_prolongate_factor_ = param.getDefault("linsolver_prolongate_factor", linsolver_prolongate_factor_);
@@ -96,7 +96,7 @@ namespace Opm
         // Ensure sufficient grid support for requested boundary
         // condition type.
         //
-	Opm::parameter::ParameterGroup temp_param = param;
+        Opm::parameter::ParameterGroup temp_param = param;
         if (bctype_ == Linear || bctype_ == Periodic) {
             if (!temp_param.has("use_unique_boundary_ids")) {
                 temp_param.insertParameter("use_unique_boundary_ids", "true");
@@ -108,8 +108,8 @@ namespace Opm
             }
         }
 
-	setupGridAndProps(temp_param, grid_, res_prop_);
-	ginterf_.init(grid_);
+        setupGridAndProps(temp_param, grid_, res_prop_);
+        ginterf_.init(grid_);
     }
 
 
@@ -118,10 +118,10 @@ namespace Opm
     template <class Traits>
     inline void UpscalerBase<Traits>::initFinal(const Opm::parameter::ParameterGroup& param)
     {
-	// Report any unused parameters.
-	std::cout << "====================   Unused parameters:   ====================\n";
-	param.displayUsage();
-	std::cout << "================================================================\n";
+        // Report any unused parameters.
+        std::cout << "====================   Unused parameters:   ====================\n";
+        param.displayUsage();
+        std::cout << "================================================================\n";
     }
 
 
@@ -140,27 +140,27 @@ namespace Opm
                                            double linsolver_prolongate_factor,
                                            int linsolver_smooth_steps)
     {
-	bctype_ = bctype;
-	residual_tolerance_ = residual_tolerance;
-	linsolver_verbosity_ = linsolver_verbosity;
+        bctype_ = bctype;
+        residual_tolerance_ = residual_tolerance;
+        linsolver_verbosity_ = linsolver_verbosity;
         linsolver_type_ = linsolver_type;
-	    linsolver_maxit_ = linsolver_maxit;
-         linsolver_prolongate_factor_ = linsolver_prolongate_factor;
+        linsolver_maxit_ = linsolver_maxit;
+        linsolver_prolongate_factor_ = linsolver_prolongate_factor;
         linsolver_smooth_steps_ = linsolver_smooth_steps;
-	twodim_hack_ = twodim_hack;
+        twodim_hack_ = twodim_hack;
 
-	// Faking some parameters depending on bc type.
+        // Faking some parameters depending on bc type.
         bool periodic_ext = (bctype_ == Periodic);
         bool turn_normals = false;
         bool clip_z = (bctype_ == Periodic);
         bool unique_bids = (bctype_ == Linear || bctype_ == Periodic);
         std::string rock_list("no_list");
-	setupGridAndPropsEclipse(parser, z_tolerance,
+        setupGridAndPropsEclipse(parser, z_tolerance,
                                  periodic_ext, turn_normals, clip_z, unique_bids,
                                  perm_threshold, rock_list,
                                  useJ<ResProp>(), 1.0, 0.0,
                                  grid_, res_prop_);
-	ginterf_.init(grid_);
+        ginterf_.init(grid_);
     }
 
 
@@ -170,7 +170,7 @@ namespace Opm
     inline const typename UpscalerBase<Traits>::GridType&
     UpscalerBase<Traits>::grid() const
     {
-	return grid_;
+        return grid_;
     }
 
 
@@ -183,7 +183,7 @@ namespace Opm
         if ((type == Periodic && bctype_ != Periodic)
             || (type != Periodic && bctype_ == Periodic)) {
             OPM_THROW(std::runtime_error, "Cannot switch to or from Periodic boundary condition, "
-                  "periodic must be set in init() params.");
+                      "periodic must be set in init() params.");
         } else {
             bctype_ = type;
             if (type == Periodic || type == Linear) {
@@ -223,56 +223,56 @@ namespace Opm
     inline typename UpscalerBase<Traits>::permtensor_t
     UpscalerBase<Traits>::upscaleEffectivePerm(const FluidInterface& fluid)
     {
-	int num_cells = ginterf_.numberOfCells();
-	// No source or sink.
-	std::vector<double> src(num_cells, 0.0);
-	// Just water.
-	std::vector<double> sat(num_cells, 1.0);
-	// Gravity.
-	Dune::FieldVector<double, 3> gravity(0.0);
-	// gravity[2] = -Dune::unit::gravity;
+        int num_cells = ginterf_.numberOfCells();
+        // No source or sink.
+        std::vector<double> src(num_cells, 0.0);
+        // Just water.
+        std::vector<double> sat(num_cells, 1.0);
+        // Gravity.
+        Dune::FieldVector<double, 3> gravity(0.0);
+        // gravity[2] = -Dune::unit::gravity;
 
-	permtensor_t upscaled_K(3, 3, (double*)0);
-	for (int pdd = 0; pdd < Dimension; ++pdd) {
-	    setupUpscalingConditions(ginterf_, bctype_, pdd, 1.0, 1.0, twodim_hack_, bcond_);
-	    if (pdd == 0) {
-		// Only on first iteration, since we do not change the
-		// structure of the system, the way the flow solver is
-		// implemented.
-		flow_solver_.init(ginterf_, res_prop_, gravity, bcond_);
-	    }
-
-	    // Run pressure solver.
+        permtensor_t upscaled_K(3, 3, (double*)0);
+        for (int pdd = 0; pdd < Dimension; ++pdd) {
+            setupUpscalingConditions(ginterf_, bctype_, pdd, 1.0, 1.0, twodim_hack_, bcond_);
+            if (pdd == 0) {
+                // Only on first iteration, since we do not change the
+                // structure of the system, the way the flow solver is
+                // implemented.
+                flow_solver_.init(ginterf_, res_prop_, gravity, bcond_);
+            }
+            
+            // Run pressure solver.
             bool same_matrix = (bctype_ != Fixed) && (pdd != 0);
-	    flow_solver_.solve(fluid, sat, bcond_, src, residual_tolerance_,
+            flow_solver_.solve(fluid, sat, bcond_, src, residual_tolerance_,
                                linsolver_verbosity_, 
                                linsolver_type_, same_matrix,
                                linsolver_maxit_, linsolver_prolongate_factor_,
                                linsolver_smooth_steps_);
             double max_mod = flow_solver_.postProcessFluxes();
             std::cout << "Max mod = " << max_mod << std::endl;
-
-	    // Compute upscaled K.
-	    double Q[Dimension] =  { 0 };
-	    switch (bctype_) {
-	    case Fixed:
-		Q[pdd] = computeAverageVelocity(flow_solver_.getSolution(), pdd, pdd);
-		break;
-	    case Linear:
-	    case Periodic:
-		for (int i = 0; i < Dimension; ++i) {
-		    Q[i] = computeAverageVelocity(flow_solver_.getSolution(), i, pdd);
-		}
-		break;
-	    default:
-		OPM_THROW(std::runtime_error, "Unknown boundary type: " << bctype_);
-	    }
-	    double delta = computeDelta(pdd);
-	    for (int i = 0; i < Dimension; ++i) {
-		upscaled_K(i, pdd) = Q[i] * delta;
-	    }
-	}
-	return upscaled_K;
+            
+            // Compute upscaled K.
+            double Q[Dimension] =  { 0 };
+            switch (bctype_) {
+            case Fixed:
+                Q[pdd] = computeAverageVelocity(flow_solver_.getSolution(), pdd, pdd);
+                break;
+            case Linear:
+            case Periodic:
+                for (int i = 0; i < Dimension; ++i) {
+                    Q[i] = computeAverageVelocity(flow_solver_.getSolution(), i, pdd);
+                }
+                break;
+            default:
+                OPM_THROW(std::runtime_error, "Unknown boundary type: " << bctype_);
+            }
+            double delta = computeDelta(pdd);
+            for (int i = 0; i < Dimension; ++i) {
+                upscaled_K(i, pdd) = Q[i] * delta;
+            }
+        }
+        return upscaled_K;
     }
 
 
@@ -281,94 +281,94 @@ namespace Opm
     template <class Traits>
     template <class FlowSol>
     double UpscalerBase<Traits>::computeAverageVelocity(const FlowSol& flow_solution,
-                                                               const int flow_dir,
-                                                               const int pdrop_dir) const
+                                                        const int flow_dir,
+                                                        const int pdrop_dir) const
     {
-	double side1_flux = 0.0;
-	double side2_flux = 0.0;
-	double side1_area = 0.0;
-	double side2_area = 0.0;
-
-	int num_faces = 0;
-	int num_bdyfaces = 0;
-	int num_side1 = 0;
-	int num_side2 = 0;
-
-	for (CellIter c = ginterf_.cellbegin(); c != ginterf_.cellend(); ++c) {
-	    for (FaceIter f = c->facebegin(); f != c->faceend(); ++f) {
-		++num_faces;
-		if (f->boundary()) {
-		    ++num_bdyfaces;
-		    int canon_bid = bcond_.getCanonicalBoundaryId(f->boundaryId());
-		    if ((canon_bid - 1)/2 == flow_dir) {
-			double flux = flow_solution.outflux(f);
-			double area = f->area();
-			double norm_comp = f->normal()[flow_dir];
-			// std::cout << "bid " << f->boundaryId() << "   area " << area << "   n " << norm_comp << std::endl;
-			if (canon_bid - 1 == 2*flow_dir) {
-			    ++num_side1;
-			    if (flow_dir == pdrop_dir && flux > 0.0) {
+        double side1_flux = 0.0;
+        double side2_flux = 0.0;
+        double side1_area = 0.0;
+        double side2_area = 0.0;
+        
+        int num_faces = 0;
+        int num_bdyfaces = 0;
+        int num_side1 = 0;
+        int num_side2 = 0;
+        
+        for (CellIter c = ginterf_.cellbegin(); c != ginterf_.cellend(); ++c) {
+            for (FaceIter f = c->facebegin(); f != c->faceend(); ++f) {
+                ++num_faces;
+                if (f->boundary()) {
+                    ++num_bdyfaces;
+                    int canon_bid = bcond_.getCanonicalBoundaryId(f->boundaryId());
+                    if ((canon_bid - 1)/2 == flow_dir) {
+                        double flux = flow_solution.outflux(f);
+                        double area = f->area();
+                        double norm_comp = f->normal()[flow_dir];
+                        // std::cout << "bid " << f->boundaryId() << "   area " << area << "   n " << norm_comp << std::endl;
+                        if (canon_bid - 1 == 2*flow_dir) {
+                            ++num_side1;
+                            if (flow_dir == pdrop_dir && flux > 0.0) {
 #ifdef VERBOSE
-			      std::cerr << "Flow may be in wrong direction at bid: " << f->boundaryId()<<" (canonical: "<<canon_bid
-					  << ") Magnitude: " << std::fabs(flux) << std::endl;
+                                std::cerr << "Flow may be in wrong direction at bid: " << f->boundaryId()<<" (canonical: "<<canon_bid
+                                          << ") Magnitude: " << std::fabs(flux) << std::endl;
 #endif
-				// OPM_THROW(std::runtime_error, "Detected outflow at entry face: " << face);
-			    }
-			    side1_flux += flux*norm_comp;
-			    side1_area += area;
-			} else {
-			    assert(canon_bid - 1 == 2*flow_dir + 1);
-			    ++num_side2;
-			    if (flow_dir == pdrop_dir && flux < 0.0) {
+                                // OPM_THROW(std::runtime_error, "Detected outflow at entry face: " << face);
+                            }
+                            side1_flux += flux*norm_comp;
+                            side1_area += area;
+                        } else {
+                            assert(canon_bid - 1 == 2*flow_dir + 1);
+                            ++num_side2;
+                            if (flow_dir == pdrop_dir && flux < 0.0) {
 #ifdef VERBOSE
-				std::cerr << "Flow may be in wrong direction at bid: " << f->boundaryId()
-					  << " Magnitude: " << std::fabs(flux) << std::endl;
+                                std::cerr << "Flow may be in wrong direction at bid: " << f->boundaryId()
+                                          << " Magnitude: " << std::fabs(flux) << std::endl;
 #endif
-				// OPM_THROW(std::runtime_error, "Detected inflow at exit face: " << face);
-			    }
-			    side2_flux += flux*norm_comp;
-			    side2_area += area;
-			}
-		    }		    
-		}
-	    }
-	}
-// 	std::cout << "Faces: " << num_faces << "   Boundary faces: " << num_bdyfaces
-// 		  << "   Side 1 faces: " << num_side1 << "   Side 2 faces: " << num_side2 << std::endl;
-	// q is the average velocity.
-	return 0.5*(side1_flux/side1_area + side2_flux/side2_area);
+                                // OPM_THROW(std::runtime_error, "Detected inflow at exit face: " << face);
+                            }
+                            side2_flux += flux*norm_comp;
+                            side2_area += area;
+                        }
+                    }               
+                }
+            }
+        }
+        //      std::cout << "Faces: " << num_faces << "   Boundary faces: " << num_bdyfaces
+        //                << "   Side 1 faces: " << num_side1 << "   Side 2 faces: " << num_side2 << std::endl;
+        // q is the average velocity.
+        return 0.5*(side1_flux/side1_area + side2_flux/side2_area);
     }
-
-
+    
+    
 
 
     template <class Traits>
     inline double UpscalerBase<Traits>::computeDelta(const int flow_dir) const
     {
-	double side1_pos = 0.0;
-	double side2_pos = 0.0;
-	double side1_area = 0.0;
-	double side2_area = 0.0;
-	for (CellIter c = ginterf_.cellbegin(); c != ginterf_.cellend(); ++c) {
-	    for (FaceIter f = c->facebegin(); f != c->faceend(); ++f) {
-		if (f->boundary()) {
-		    int canon_bid = bcond_.getCanonicalBoundaryId(f->boundaryId());
-		    if ((canon_bid - 1)/2 == flow_dir) {
-			double area = f->area();
-			double pos_comp = f->centroid()[flow_dir];
-			if (canon_bid - 1 == 2*flow_dir) {
-			    side1_pos += area*pos_comp;
-			    side1_area += area;
-			} else {
-			    side2_pos += area*pos_comp;
-			    side2_area += area;
-			}
-		    }		    
-		}
-	    }
-	}
-	// delta is the average length.
-	return  side2_pos/side2_area - side1_pos/side1_area;
+        double side1_pos = 0.0;
+        double side2_pos = 0.0;
+        double side1_area = 0.0;
+        double side2_area = 0.0;
+        for (CellIter c = ginterf_.cellbegin(); c != ginterf_.cellend(); ++c) {
+            for (FaceIter f = c->facebegin(); f != c->faceend(); ++f) {
+                if (f->boundary()) {
+                    int canon_bid = bcond_.getCanonicalBoundaryId(f->boundaryId());
+                    if ((canon_bid - 1)/2 == flow_dir) {
+                        double area = f->area();
+                        double pos_comp = f->centroid()[flow_dir];
+                        if (canon_bid - 1 == 2*flow_dir) {
+                            side1_pos += area*pos_comp;
+                            side1_area += area;
+                        } else {
+                            side2_pos += area*pos_comp;
+                            side2_area += area;
+                        }
+                    }               
+                }
+            }
+        }
+        // delta is the average length.
+        return  side2_pos/side2_area - side1_pos/side1_area;
     }
 
 
@@ -379,7 +379,7 @@ namespace Opm
     {
         double total_vol = 0.0;
         double total_pore_vol = 0.0;
-	for (CellIter c = ginterf_.cellbegin(); c != ginterf_.cellend(); ++c) {
+        for (CellIter c = ginterf_.cellbegin(); c != ginterf_.cellend(); ++c) {
             total_vol += c->volume();
             total_pore_vol += c->volume()*res_prop_.porosity(c->index());
         }
@@ -392,7 +392,7 @@ namespace Opm
     {
         double total_vol = 0.0;
         double total_pore_vol = 0.0;
-	for (CellIter c = ginterf_.cellbegin(); c != ginterf_.cellend(); ++c) {
+        for (CellIter c = ginterf_.cellbegin(); c != ginterf_.cellend(); ++c) {
             total_vol += c->volume();
             total_pore_vol += c->volume()*res_prop_.porosity(c->index())*res_prop_.ntg(c->index());
         }
@@ -404,13 +404,13 @@ namespace Opm
     {
         double total_vol = 0.0;
         double total_net_vol = 0.0;
-	for (CellIter c = ginterf_.cellbegin(); c != ginterf_.cellend(); ++c) {
+        for (CellIter c = ginterf_.cellbegin(); c != ginterf_.cellend(); ++c) {
             total_vol += c->volume();
             total_net_vol += c->volume()*res_prop_.ntg(c->index());
         }
         return total_net_vol/total_vol;
     }
-
+    
 } // namespace Opm
 
 
