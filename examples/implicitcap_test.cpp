e04eea53e6736434b18d1cde287143141d9de7e9
Robert Kloefkorn <robertk@posteo.org>
2014-08-07 13:00:25 +0200

    include version.hh when DUNE_VERSION_NEWER is used.

--- examples/implicitcap_test.cpp
+++ examples/implicitcap_test.cpp
@@ -42,6 +42,8 @@
 #include "SimulatorTester.hpp"
 #include "SimulatorTesterFlexibleBC.hpp"
 #include <opm/porsol/common/SimulatorTraits.hpp>
+
+#include <dune/common/version.hh>
 #if DUNE_VERSION_NEWER(DUNE_COMMON, 2, 3)
 #include <dune/common/parallel/mpihelper.hh>
 #else
