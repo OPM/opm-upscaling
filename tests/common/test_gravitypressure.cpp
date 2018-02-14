313263f55b787df55bbb10452c4c63cc92e600e5
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2018-02-14 16:05:48 +0100

    changed: let the build system handle the dynamic boost test defines

--- tests/common/test_gravitypressure.cpp
+++ tests/common/test_gravitypressure.cpp
@@ -19,10 +19,6 @@
 
 #include <config.h>
 
-#if HAVE_DYNAMIC_BOOST_TEST
-#define BOOST_TEST_DYN_LINK
-#endif  // HAVE_DYNAMIC_BOOST_TEST
-
 #define BOOST_TEST_MODULE UpscaleHelperGravityPressure
 
 // Enable intrusive unit-testing of private property
